5f2bd836f8277f1ac623f97d2826d7fc8c00fbf7
Thom Janssen <thomjanssen128@gmail.com>
2021-11-08 18:35:04 +0100

    DemoSlides + TechDemo

---
+++ Marktplaats/demo/DemoSlides.md
@@ -0,0 +1,101 @@
+---
+marp: true
+theme: uncover
+class: 
+- invert
+---
+
+<style>
+
+html {
+  font-weight: 400;
+}
+
+section {
+  background: #f78589;
+  color:#163c40;
+  font-family:Consolas
+  
+}
+h1 {
+    text-align: left;
+    margin-left: 100px;
+    font-size: 36px;
+    max-width:650px;
+    font-weight: 400;
+  }
+  h2 {
+    position: absolute;
+    text-align: left;
+    margin-left: 100px;
+    top:200px;
+
+    font-size: 36px;
+    font-weight: 400;
+    background-color:#163c40;
+    color:#f78589;
+    
+  }
+  h2::before {
+    content: " "
+  }
+  h2::after{
+    content: " "
+  }
+span {
+  color:white;
+}
+</style>
+
+---
+
+Marktplaats <span>/ 
+/ </span>Thom
+
+---
+
+# Bij het opstarten van Marktplaats wordt de database al gevuld; <span>gebruikers</span>, <span>advertenties</span> en <span>categoriën</span> worden aangemaakt en in de database gestopt.
+
+---
+
+## Inloggen
+“There are a <span>million</span> ways it can go wrong, but only <span>one</span> to go right.”
+
+---
+
+## Mijn advertenties
+
+
+---
+
+## Mijn advertenties bekijken
+
+---
+## Een advertentie verwijderen
+<p>Validatie?</p>
+
+---
+## Een advertentie plaatsen
+
+
+---
+
+## Uitloggen
+Kom ik nog bij de spullen 
+van de gebruiker?
+
+---
+## Registratie
+
+---
+
+## Registratie
+Is de <span>gebruikersnaam</span> nog beschikbaar? 
+Is het <span>mailadres</span> al geregistreerd?
+
+---
+
+## Inloggen 
+Als de <span>nieuwe</span> gebruiker.
+
+---
---
+++ Marktplaats/src/main/java/nl/thom/marktplaats/TechDemo.java
@@ -0,0 +1,78 @@
+package nl.thom.marktplaats;
+
+import nl.thom.marktplaats.daos.GebruikerDao;
+import nl.thom.marktplaats.form.Formulier;
+import nl.thom.marktplaats.util.Bezorgwijze;
+import nl.thom.marktplaats.util.Util;
+
+import javax.persistence.EntityManager;
+import java.util.*;
+
+import static nl.thom.marktplaats.pages.Page.renderMenu;
+import static nl.thom.marktplaats.pages.Page.renderMenuOptions;
+import static nl.thom.marktplaats.pages.Page.renderMenuOptionsWithCancel;
+import static nl.thom.marktplaats.util.Util.print;
+import static nl.thom.marktplaats.util.Util.prompt;
+
+public class TechDemo {
+
+    private final List<String> items = List.of("Naam", "Leeftijd", "Gelukkig", "Nee");
+
+    public static void main(String[] args) {
+        Util.scanner = new Scanner(System.in);
+        TechDemo demo = new TechDemo();
+
+        demo.formDemo();
+
+//        demo.menuDemo();
+//        demo.menuOptionsDemo();
+//        demo.menuOptionsWithCancelDemo();
+
+//        demo.welkeBezorgwijzen();
+    }
+
+    private void formDemo() {
+        Formulier f = new Formulier();
+        f.askForm(items);
+        Map<String, String> antwoorden = f.submit();
+
+        items.stream()
+                .map(v -> v + " -> " + antwoorden.get(v))
+                .forEach(System.out::println);
+    }
+
+    private void menuDemo() {
+        renderMenu(items);
+        String a = prompt("Welke? ");
+    }
+
+    private void menuOptionsDemo() {
+        renderMenuOptions(items);
+        String a = prompt("Welke? ");
+    }
+
+    private void menuOptionsWithCancelDemo() {
+        renderMenuOptionsWithCancel(items);
+        String a = prompt("Welke? ");
+    }
+
+    private void welkeBezorgwijzen(){
+        List b = Arrays.asList(Bezorgwijze.values());
+        boolean aan;
+        int mask;
+
+        for (int i = 0; i < 8; i++) {
+            mask = 1;
+            for (int j = 0; j < 3; j++) {
+                aan = (i & mask) != 0;
+
+                System.out.println(i + " " + b.get(j)+ " " + aan);
+
+                mask=mask<<1;
+
+            }
+
+        }
+
+    }
+}
